caef46d2283a114559c48099e3fed4a011e82acd
Freddy Contreras <freddycontreras3@gmail.com>
2022-03-15 10:47:22 -0500

    - Test svg image

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -2,31 +2,18 @@ import NextImage, { ImageProps } from "next/image";
 import { getStrapiMedia } from "lib/media";
 import { StrapiImage } from "interfaces/strapi";
 import { useMemo } from "react";
-import "external-svg-loader";
 
 const Image = ({
   src,
-  alt,
-  width,
-  height,
   ...props
-}: Omit<ImageProps, "src"> & { src: StrapiImage | string  }) => {
+}: Omit<ImageProps, "src"> & { src: StrapiImage | string }) => {
   const srcImg: string = useMemo(() => {
     return typeof src === "string"
       ? process.env.NEXT_PUBLIC_STRAPI_API_URL + src
       : (getStrapiMedia(src as StrapiImage) as string);
   }, []);
 
-  return srcImg.includes(".svg") ? (
-    <svg
-      data-src={srcImg}
-      fill="currentColor"
-      width={width} 
-      height={height}
-    />
-  ) : (
-    <NextImage src={srcImg} width={width} height={height} {...props} />
-  );
+  return <NextImage src={srcImg} {...props} />
 };
 
 export default Image;
