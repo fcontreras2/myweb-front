975dcb9bd5ef6cf8b41e0a972995dfe4e36cf8f4
Freddy Contreras <freddycontreras3@gmail.com>
2022-03-15 10:30:34 -0500

    - Test load external svg

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,33 +1,31 @@
 import NextImage, { ImageProps } from "next/image";
 import { getStrapiMedia } from "lib/media";
 import { StrapiImage } from "interfaces/strapi";
-import LazyLoad from "react-lazyload";
 import { useMemo } from "react";
+import "external-svg-loader";
 
 const Image = ({
   src,
   alt,
+  width,
+  height,
   ...props
-}: Omit<ImageProps, "src"> & { src: StrapiImage | string }) => {
-
-  const srcImg:string = useMemo(() => {
+}: Omit<ImageProps, "src"> & { src: StrapiImage | string  }) => {
+  const srcImg: string = useMemo(() => {
     return typeof src === "string"
-    ? (process.env.NEXT_PUBLIC_STRAPI_API_URL) + src
-    : (getStrapiMedia(src as StrapiImage) as string);
-  }, [])
+      ? process.env.NEXT_PUBLIC_STRAPI_API_URL + src
+      : (getStrapiMedia(src as StrapiImage) as string);
+  }, []);
 
-  return (
-
-    srcImg.includes('.svg') ? 
-    <LazyLoad>
-      <img
-        src={srcImg}
-        alt={alt || ""}
-        {...props}
-      />
-    </LazyLoad>: 
-    <NextImage src={srcImg} {...props}/>
-    
+  return srcImg.includes(".svg") ? (
+    <svg
+      data-src={srcImg}
+      fill="currentColor"
+      width={width} 
+      height={height}
+    />
+  ) : (
+    <NextImage src={srcImg} width={width} height={height} {...props} />
   );
 };
 
