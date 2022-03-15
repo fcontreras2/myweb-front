e7abd69e86e007ca95b44de6610ca57034ad882b
Freddy Contreras <freddycontreras3@gmail.com>
2022-03-15 10:17:09 -0500

    - Update image

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,22 +1,33 @@
 import NextImage, { ImageProps } from "next/image";
 import { getStrapiMedia } from "lib/media";
 import { StrapiImage } from "interfaces/strapi";
+import LazyLoad from "react-lazyload";
+import { useMemo } from "react";
 
 const Image = ({
   src,
-  width,
-  height,
   alt,
   ...props
 }: Omit<ImageProps, "src"> & { src: StrapiImage | string }) => {
+
+  const srcImg:string = useMemo(() => {
+    return typeof src === "string"
+    ? (process.env.NEXT_PUBLIC_STRAPI_API_URL) + src
+    : (getStrapiMedia(src as StrapiImage) as string);
+  }, [])
+
   return (
-    <NextImage
-      width={width}
-      height={height}
-      src={typeof src === "string" ? (process.env.NODE_ENV === 'development' ? 'http://localhost:1337' : process.env.NEXT_PUBLIC_STRAPI_API_URL) +  src : getStrapiMedia(src as StrapiImage) as string}
-      alt={alt || ""}
-      {...props}
-    />
+
+    srcImg.includes('.svg') ? 
+    <LazyLoad>
+      <img
+        src={srcImg}
+        alt={alt || ""}
+        {...props}
+      />
+    </LazyLoad>: 
+    <NextImage src={srcImg} {...props}/>
+    
   );
 };
 
